772a85674a2330cb299b823f011a422537d1a64a
Rugi <daniel.ruginets@gmail.com>
2018-07-10 22:04:25 -0400

    updated some stuff

--- veganambition/src/components/HelloWorld.vue
+++ veganambition/src/components/HelloWorld.vue
@@ -1,35 +1,106 @@
 <template>
 
   <div class="hello">
-    <div class="container is-fluid">
+    <div class="container is-desktop">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
     <p>Hosted by <a href="https://www.danielruginets.com">Daniel Ruginets</a></p>
     <p class="column is-three-fifths is-offset-one-fifth">A podcast series hosted by some dude named Rugi. We will feature guests with different diets. Different sections will split up the podcast, and every week we will have a debate period with the guest, a talk about each others diet and differences about them, taste test a vegan food, and feature a vegan recipe.</p>
-    <div class="notifcation">
-    <div class="columns is-centered is-mobile">
-      <div class="column"></div>
-      <div class="column"></div>
-      <div class="column"></div>
-      <div class="column"></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://itunes.apple.com/us/podcast/the-vegan-ambition/id1389810728?mt=2" data-vars-outbound-link="https://itunes.apple.com/us/podcast/the-vegan-ambition/id1389810728?mt=2" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts@2x.png 2x" alt="Apple Podcasts Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8zZDQwMjZjL3BvZGNhc3QvcnNz" data-vars-outbound-link="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8zZDQwMjZjL3BvZGNhc3QvcnNz" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts@2x.png 2x" alt="Google Podcasts Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://www.breaker.audio/the-vegan-ambition" data-vars-outbound-link="https://www.breaker.audio/the-vegan-ambition" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker@2x.png 2x" alt="Breaker Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://castbox.fm/channel/id1310808" data-vars-outbound-link="https://castbox.fm/channel/id1310808" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox@2x.png 2x" alt="Castbox Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&amp;isi=691797987&amp;ius=googleplaymusic&amp;apn=com.google.android.music&amp;link=https://play.google.com/music/m/Ix6qmn27kh433jiyiicvgcxscu4?t%3DThe_Vegan_Ambition%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16" data-vars-outbound-link="https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&amp;isi=691797987&amp;ius=googleplaymusic&amp;apn=com.google.android.music&amp;link=https://play.google.com/music/m/Ix6qmn27kh433jiyiicvgcxscu4?t%3DThe_Vegan_Ambition%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play@2x.png 2x" alt="Google Play Music Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://pca.st/ECel" data-vars-outbound-link="https://pca.st/ECel" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts@2x.png 2x" alt="Pocket Casts Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://www.podbean.com/podcast-detail/n62ei-6df96/The-Vegan-Ambition-Podcast" data-vars-outbound-link="https://www.podbean.com/podcast-detail/n62ei-6df96/The-Vegan-Ambition-Podcast" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean@2x.png 2x" alt="PodBean Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://play.radiopublic.com/the-vegan-ambition-6pLZ51" data-vars-outbound-link="https://play.radiopublic.com/the-vegan-ambition-6pLZ51" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic@2x.png 2x" alt="Radio Public Logo" height="32" width="32"></a></div>
-    <div class="column"><a class=" jsOutboundLink" href="https://www.stitcher.com/podcast/anchor-podcasts/the-vegan-ambition" data-vars-outbound-link="https://www.stitcher.com/podcast/anchor-podcasts/the-vegan-ambition" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher@2x.png 2x" alt="Stitcher Logo" height="32" width="32"></a></div>
-    <div class="column"></div>
-    <div class="column"></div>
-    <div class="column"></div>
-    <div class="column"></div>
+<section class="section section-padding">
+<nav class="level">
+  <div class="level-item has-text-centered ">
+    <div>
+    <p class="heading">Apple Podcasts</p>
+<a href="https://itunes.apple.com/us/podcast/the-vegan-ambition/id1389810728?mt=2" data-vars-outbound-link="https://itunes.apple.com/us/podcast/the-vegan-ambition/id1389810728?mt=2" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/apple_podcasts@2x.png 2x" alt="Apple Podcasts Logo" height="32" width="32"></a>
     </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Google Podcasts</p>
+<a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8zZDQwMjZjL3BvZGNhc3QvcnNz" data-vars-outbound-link="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hbmNob3IuZm0vcy8zZDQwMjZjL3BvZGNhc3QvcnNz" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_podcasts@2x.png 2x" alt="Google Podcasts Logo" height="32" width="32"></a>
     </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Breaker</p>
+<a href="https://www.breaker.audio/the-vegan-ambition" data-vars-outbound-link="https://www.breaker.audio/the-vegan-ambition" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/breaker@2x.png 2x" alt="Breaker Logo" height="32" width="32"></a>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Castbox</p>
+<a href="https://castbox.fm/channel/id1310808" data-vars-outbound-link="https://castbox.fm/channel/id1310808" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/castbox@2x.png 2x" alt="Castbox Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Google Play Music</p>
+<a href="https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&amp;isi=691797987&amp;ius=googleplaymusic&amp;apn=com.google.android.music&amp;link=https://play.google.com/music/m/Ix6qmn27kh433jiyiicvgcxscu4?t%3DThe_Vegan_Ambition%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16" data-vars-outbound-link="https://playmusic.app.goo.gl/?ibi=com.google.PlayMusic&amp;isi=691797987&amp;ius=googleplaymusic&amp;apn=com.google.android.music&amp;link=https://play.google.com/music/m/Ix6qmn27kh433jiyiicvgcxscu4?t%3DThe_Vegan_Ambition%26pcampaignid%3DMKT-na-all-co-pr-mu-pod-16" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/google_play@2x.png 2x" alt="Google Play Music Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Pocket Casts</p>
+<a href="https://pca.st/ECel" data-vars-outbound-link="https://pca.st/ECel" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/pocket_casts@2x.png 2x" alt="Pocket Casts Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Podbean</p>
+<a href="https://www.podbean.com/podcast-detail/n62ei-6df96/The-Vegan-Ambition-Podcast" data-vars-outbound-link="https://www.podbean.com/podcast-detail/n62ei-6df96/The-Vegan-Ambition-Podcast" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/podbean@2x.png 2x" alt="PodBean Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Radio Republic</p>
+<a href="https://play.radiopublic.com/the-vegan-ambition-6pLZ51" data-vars-outbound-link="https://play.radiopublic.com/the-vegan-ambition-6pLZ51" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/radiopublic@2x.png 2x" alt="Radio Public Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Sticher</p>
+<a href="https://www.stitcher.com/podcast/anchor-podcasts/the-vegan-ambition" data-vars-outbound-link="https://www.stitcher.com/podcast/anchor-podcasts/the-vegan-ambition" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher.png" srcset="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/stitcher@2x.png 2x" alt="Stitcher Logo" height="32" width="32"></a>
+    </div>
+  </div>
+    <div class="level-item has-text-centered">
+    <div>
+    <p class="heading">Spotify</p>
+<a href="https://open.spotify.com/show/1RTMLy8r72xxx34xEFB90J" data-vars-outbound-link="https://open.spotify.com/show/1RTMLy8r72xxx34xEFB90J" target="_blank" rel="noopener noreferrer"><img src="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/spotify.png" srcSet="https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/spotify.png 1x, https://d12xoj7p9moygp.cloudfront.net/images/podcast/logo-square/004/spotify@2x.png 2x" alt="Spotify Logo" height="32" width="32"/></a>
+    </div>
+  </div>
+</nav>
+</section>
+<section class="section section-padding">
     <iframe src="https://anchor.fm/vegan-ambition/embed" height="102px" width="400px" frameborder="0" scrolling="no"></iframe>
-    
+    </section>
     </div>
-<div class="fix-footer">
+<div>
+
+<nav class="level">
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Listens</p>
+      <p class="title">196</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Instagram Followers</p>
+      <p class="title">310</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Posts</p>
+      <p class="title">16</p>
+    </div>
+  </div>
+  <div class="level-item has-text-centered">
+    <div>
+      <p class="heading">Following</p>
+      <p class="title">444</p>
+    </div>
+  </div>
+</nav>
     <footer class="footer">
   <div class="content has-text-centered">
 
@@ -86,14 +157,6 @@ li {
 a {
   color: #42b983;
 }
-
-.fix-footer {
-    position: absolute;
-    width: 100%;
-    bottom: 0;
-    overflow:hidden;
-}
-
 .footer{
   padding-bottom: 0.5rem !important;
   padding-top: 2rem !important;
